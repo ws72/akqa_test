cf4ed33fe3142c671223d4aa9843efac5346ca95
Song Wang <song.wang.au@gmail.com>
2018-02-19 12:31:22 +1100

    Fixed webpack issue.

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,42 @@
-const htmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+var htmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: __dirname + '/dist',
-    filename: 'bundle.js'
+module.exports={
+  entry:'./src/index.js',
+  output:{
+    path:__dirname+'/dist',
+    filename:'bundle.js'
   },
-  plugins: [
+  plugins:[
     new htmlWebpackPlugin({
-      template: 'index.html'
+      template:'index.html'
     })
   ],
-  devServer: {
-    inline: true,
-    contentBase: './dist',
-    port: 3000
+  devServer:{
+    inline:true,
+    contentBase:'./dist',
+    port:3000
   },
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: "babel-loader",
-      query: {
-        presets: ['react', 'es2015']
+  module:{
+    loaders:[{
+      test:/\.js$/,
+      exclude:/node_modules/,
+      loader:"babel-loader",
+      query:{
+        presets:['react','es2015']
       }
     },
     {
-      test: /\.css$/,
+      test:/\.css$/,
       loader: "style-loader!css-loader"
     },
     {
-      test: /\.scss$/,
-      loader: 'style-loader!css-loader!sass-loader'
+      test:/\.scss$/,
+      loader:'style-loader!css-loader!sass-loader'
     },
     {
-      test: /\.json$/,
-      loader: 'json-loader'
+      test:/\.json$/,
+      loader:'json-loader'
     }
-    ]
-  },
-
-
-  plugins: [
-    new CopyWebpackPlugin([
-      { from: 'src/assets/img', to: 'img' },
-    ])]
+  ]
+  }
 }
